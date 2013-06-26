6452a26df11479157211eeffc4d446686850e28e
praznav <Pranav@gollum.(none)>
2013-06-26 11:55:21 -0400

    awesome stuff

---
+++ New Project/New Project/New Project.cpp
@@ -0,0 +1,29 @@
+// New Project.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <Windows.h>
+#include <NuiApi.h>
+using namespace std;
+
+int main()
+{   
+	cout << "Hello world" << endl;
+	NuiInitialize(NUI_INITIALIZE_FLAG_USES_SKELETON);
+	NUI_SKELETON_FRAME ourframe;
+	while (1) //For all of time
+	{
+		NuiSkeletonGetNextFrame(0, &ourframe); //Get a frame and stuff it into ourframe
+		for (int i = 0; i < 6; i++) //Six times
+		{
+			if (ourframe.SkeletonData[i].eTrackingState == NUI_SKELETON_TRACKED) //See more on this line below
+			cout << "Right Hand: "; //Print "Right hand:"
+			cout << ourframe.SkeletonData[i].SkeletonPositions[NUI_SKELETON_POSITION_HAND_RIGHT].y << endl; //See more on this line below
+		}
+		system("cls");//Clear the screen
+	}
+	NuiShutdown();
+	return 0;
+}
+
