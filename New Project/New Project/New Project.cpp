6d5ac582a6e896e5473b46d84cd2fb30ad8b23b2
praznav <Pranav@gollum.(none)>
2013-06-27 13:15:14 -0400

    ok

--- New Project/New Project/New Project.cpp
+++ New Project/New Project/New Project.cpp
@@ -45,7 +45,7 @@ struct Skeleton
 	} 
 } body;
 
-Skeleton bodyArray[100];
+Skeleton bodyArray[1000];
 
 void printSkeleton(Skeleton skel);
 void printVector3D(Vector3D vec);
@@ -55,10 +55,10 @@ int main()
 	cout << "Hello world" << endl;
 	NuiInitialize(NUI_INITIALIZE_FLAG_USES_SKELETON);
 	NUI_SKELETON_FRAME ourframe;
-	while (1) //For all of time
+	while (true) //For all of time
 	{
-		body = Skeleton();
-
+		Skeleton*body = new Skeleton();
+		
 		NuiSkeletonGetNextFrame(0, &ourframe); //Get a frame and stuff it into ourframe
 		for (int j = 0; j <= 9; j++)
 		{
@@ -68,27 +68,27 @@ int main()
 		   
 			if (ourframe.SkeletonData[i].eTrackingState == NUI_SKELETON_TRACKED) //See more on this line below
 			{
-			   body.hip.x += ourframe.SkeletonData[i].SkeletonPositions[NUI_SKELETON_POSITION_HIP_CENTER].x;
-			   body.hip.y += ourframe.SkeletonData[i].SkeletonPositions[NUI_SKELETON_POSITION_HIP_CENTER].y;
-			   body.hip.z += ourframe.SkeletonData[i].SkeletonPositions[NUI_SKELETON_POSITION_HIP_CENTER].z;
-			   body.head.x += ourframe.SkeletonData[i].SkeletonPositions[NUI_SKELETON_POSITION_HEAD].x;
-			   body.head.y += ourframe.SkeletonData[i].SkeletonPositions[NUI_SKELETON_POSITION_HEAD].y;
-			   body.head.z += ourframe.SkeletonData[i].SkeletonPositions[NUI_SKELETON_POSITION_HEAD].z;
-			   body.spine.x += ourframe.SkeletonData[i].SkeletonPositions[NUI_SKELETON_POSITION_SPINE].x;
-			   body.spine.y += ourframe.SkeletonData[i].SkeletonPositions[NUI_SKELETON_POSITION_SPINE].y;
-			   body.spine.z += ourframe.SkeletonData[i].SkeletonPositions[NUI_SKELETON_POSITION_SPINE].z;
-			   body.leftfoot.x += ourframe.SkeletonData[i].SkeletonPositions[NUI_SKELETON_POSITION_FOOT_LEFT].x;
-			   body.leftfoot.y += ourframe.SkeletonData[i].SkeletonPositions[NUI_SKELETON_POSITION_FOOT_LEFT].y;
-			   body.leftfoot.z += ourframe.SkeletonData[i].SkeletonPositions[NUI_SKELETON_POSITION_FOOT_LEFT].z;
-			   body.rightfoot.x += ourframe.SkeletonData[i].SkeletonPositions[NUI_SKELETON_POSITION_FOOT_RIGHT].x;
-			   body.rightfoot.y += ourframe.SkeletonData[i].SkeletonPositions[NUI_SKELETON_POSITION_FOOT_RIGHT].y;
-			   body.rightfoot.z += ourframe.SkeletonData[i].SkeletonPositions[NUI_SKELETON_POSITION_FOOT_RIGHT].z;
-			   body.leftknee.x += ourframe.SkeletonData[i].SkeletonPositions[NUI_SKELETON_POSITION_KNEE_LEFT].x;
-			   body.leftknee.y += ourframe.SkeletonData[i].SkeletonPositions[NUI_SKELETON_POSITION_KNEE_LEFT].y;
-			   body.leftknee.z += ourframe.SkeletonData[i].SkeletonPositions[NUI_SKELETON_POSITION_KNEE_LEFT].z;
-			   body.rightknee.x += ourframe.SkeletonData[i].SkeletonPositions[NUI_SKELETON_POSITION_KNEE_RIGHT].x;
-			   body.rightknee.y += ourframe.SkeletonData[i].SkeletonPositions[NUI_SKELETON_POSITION_KNEE_RIGHT].y;
-			   body.rightknee.z += ourframe.SkeletonData[i].SkeletonPositions[NUI_SKELETON_POSITION_KNEE_RIGHT].z;
+			   body->hip.x += ourframe.SkeletonData[i].SkeletonPositions[NUI_SKELETON_POSITION_HIP_CENTER].x;	   		
+			   body->hip.y += ourframe.SkeletonData[i].SkeletonPositions[NUI_SKELETON_POSITION_HIP_CENTER].y;
+			   body->hip.z += ourframe.SkeletonData[i].SkeletonPositions[NUI_SKELETON_POSITION_HIP_CENTER].z;
+			   body->head.x += ourframe.SkeletonData[i].SkeletonPositions[NUI_SKELETON_POSITION_HEAD].x;
+			   body->head.y += ourframe.SkeletonData[i].SkeletonPositions[NUI_SKELETON_POSITION_HEAD].y;
+			   body->head.z += ourframe.SkeletonData[i].SkeletonPositions[NUI_SKELETON_POSITION_HEAD].z;
+			   body->spine.x += ourframe.SkeletonData[i].SkeletonPositions[NUI_SKELETON_POSITION_SPINE].x;
+			   body->spine.y += ourframe.SkeletonData[i].SkeletonPositions[NUI_SKELETON_POSITION_SPINE].y;
+			   body->spine.z += ourframe.SkeletonData[i].SkeletonPositions[NUI_SKELETON_POSITION_SPINE].z;
+			   body->leftfoot.x += ourframe.SkeletonData[i].SkeletonPositions[NUI_SKELETON_POSITION_FOOT_LEFT].x;
+			   body->leftfoot.y += ourframe.SkeletonData[i].SkeletonPositions[NUI_SKELETON_POSITION_FOOT_LEFT].y;
+			   body->leftfoot.z += ourframe.SkeletonData[i].SkeletonPositions[NUI_SKELETON_POSITION_FOOT_LEFT].z;
+			   body->rightfoot.x += ourframe.SkeletonData[i].SkeletonPositions[NUI_SKELETON_POSITION_FOOT_RIGHT].x;
+			   body->rightfoot.y += ourframe.SkeletonData[i].SkeletonPositions[NUI_SKELETON_POSITION_FOOT_RIGHT].y;
+			   body->rightfoot.z += ourframe.SkeletonData[i].SkeletonPositions[NUI_SKELETON_POSITION_FOOT_RIGHT].z;
+			   body->leftknee.x += ourframe.SkeletonData[i].SkeletonPositions[NUI_SKELETON_POSITION_KNEE_LEFT].x;
+			   body->leftknee.y += ourframe.SkeletonData[i].SkeletonPositions[NUI_SKELETON_POSITION_KNEE_LEFT].y;
+			   body->leftknee.z += ourframe.SkeletonData[i].SkeletonPositions[NUI_SKELETON_POSITION_KNEE_LEFT].z;
+			   body->rightknee.x += ourframe.SkeletonData[i].SkeletonPositions[NUI_SKELETON_POSITION_KNEE_RIGHT].x;
+			   body->rightknee.y += ourframe.SkeletonData[i].SkeletonPositions[NUI_SKELETON_POSITION_KNEE_RIGHT].y;
+			   body->rightknee.z += ourframe.SkeletonData[i].SkeletonPositions[NUI_SKELETON_POSITION_KNEE_RIGHT].z;
 			}
 			else 
 			{
@@ -100,30 +100,31 @@ int main()
 
 
 			}
-
-	    body.hip.x /= j;
-		body.hip.y /= j;
-		body.hip.z /= j;
-		body.head.x /= j;
-		body.head.y /= j;
-		body.head.z /= j;
-		body.spine.x /= j;
-		body.spine.y /= j;
-		body.spine.z /= j;
-		body.rightfoot.x /= j;
-		body.rightfoot.y /= j;
-		body.rightfoot.z /= j;
-		body.leftfoot.x /= j;
-		body.leftfoot.y /= j;
-		body.leftfoot.z /= j;
-		body.rightknee.x /= j;
-		body.rightknee.y /= j;
-		body.rightknee.z /= j;
-		body.leftknee.x /= j;
-		body.leftknee.y /= j;
-		body.leftknee.z /= j;
-
-
+			
+			cout << body->hip.z << endl;
+			if (j == 9){
+				body->hip.x /= (j+1);
+				body->hip.y /= (j+1);
+				body->hip.z /= (j+1);
+				body->head.x /= (j+1);
+				body->head.y /= (j+1);
+				body->head.z /= (j+1);
+				body->spine.x /= (j+1);
+				body->spine.y /= (j+1);
+				body->spine.z /= (j+1);
+				body->rightfoot.x /= (j+1);
+				body->rightfoot.y /= (j+1);
+				body->rightfoot.z /= (j+1);
+				body->leftfoot.x /= (j+1);
+				body->leftfoot.y /= (j+1);
+				body->leftfoot.z /= (j+1);
+				body->rightknee.x /= (j+1);
+				body->rightknee.y /= (j+1);
+				body->rightknee.z /= (j+1);
+				body->leftknee.x /= (j+1);
+				body->leftknee.y /= (j+1);
+				body->leftknee.z /= (j+1);
+			}
   		std :: cout << "\ncounter : ";
 		std :: cout << counter;
 
@@ -134,8 +135,9 @@ int main()
 	}
 	    
 		
-		bodyArray[counter] = body;
-		if (body.hip.z <= 1.1  && counter >= 3) { 
+		bodyArray[counter] = *body;
+		if (body->hip.z <= 1.3  && counter >= 3) { 
+
 		    std :: cout << "\n";
 			//TEST CODES WILL GO HERE
 
@@ -144,7 +146,7 @@ int main()
 			for (int f = 0; f < counter; f ++) {
 				printSkeleton(bodyArray[f]);
 				cout << "\n";
-				xtotalstr += bodyArray[f].hip.x;	
+				xtotalstr += bodyArray[f].hip.x	;
 			}
 			double xtotaldev = 0;
 			for (int f = 0; f < counter; f++) 
@@ -164,26 +166,24 @@ int main()
 	}
 
 	return 0;
-
-
 }
 
 void printSkeleton(Skeleton skel)
 {
 	cout << "Hip: ";
-	printVector3D(body.hip);
+	printVector3D(skel.hip);
 	cout << "Head: ";
-	printVector3D(body.head);
+	printVector3D(skel.head);
 	cout << "Spine: ";
-	printVector3D(body.spine);
+	printVector3D(skel.spine);
 	cout << "Right Foot: ";
-	printVector3D(body.rightfoot);
+	printVector3D(skel.rightfoot);
 	cout << "Left Foot: ";
-	printVector3D(body.leftfoot);
+	printVector3D(skel.leftfoot);
 	cout << "Right Knee: ";
-	printVector3D(body.rightknee);
+	printVector3D(skel.rightknee);
 	cout << "Left Knee: ";
-	printVector3D(body.leftknee);
+	printVector3D(skel.leftknee);
 
 }
 
